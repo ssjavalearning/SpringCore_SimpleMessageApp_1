14b531701a4ce31b32a82baa477c368b614bca91
suraj singh <surajtech003@gmail.com>
2018-07-09 00:08:53 +0530

    first commit

---
+++ src/com/suraj/spring/client/ClientTest.java
@@ -0,0 +1,21 @@
+package com.suraj.spring.client;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.suraj.spring.model.HelloMessage;
+
+public class ClientTest {
+	
+	public static void main(String[] args) {
+		
+		Resource resource = new ClassPathResource("spring.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+		
+		HelloMessage hmessage = (HelloMessage)factory.getBean("helloMessage");
+		System.out.println(hmessage.getMessageId()+"\t"+hmessage.getMessage());
+		
+	}
+}
---
+++ src/com/suraj/spring/model/HelloMessage.java
@@ -0,0 +1,22 @@
+package com.suraj.spring.model;
+
+public class HelloMessage {
+
+	private int messageId;
+	private String message;
+	public int getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(int messageId) {
+		this.messageId = messageId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
